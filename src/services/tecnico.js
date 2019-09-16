84e7c14baa6614e47973c315daaecc3dbdaa1b09
jessiLeandro <jessi.profissional@gmail.com>
2019-09-16 16:14:13 -0300

    FIX

--- src/services/tecnico.js
+++ src/services/tecnico.js
@@ -51,6 +51,30 @@ export const getTecnico = async (query) => {
   return response
 } 
 
+export const getAllTecnico = async (query) => {
+  const storeObject = store.getState()
+
+  const headers = {
+    token: storeObject.auth.token,
+    username: storeObject.auth.username,
+  }
+
+  let response = {}
+
+  await axios.get(`${BACKEND_URL}/api/technician/getAllTechnician`, { headers: headers, params: { query } }).then(
+    resp => {
+      response = resp
+    }
+  ).catch((error) => {
+    if (error.response) {
+      response = error.response
+    } else {
+      console.log('Error', error.message);
+    }
+  })
+  return response
+} 
+
 export const newTecnico = async (values) => {
   const storeObject = store.getState()
 
