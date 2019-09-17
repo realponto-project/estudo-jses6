f1315b782d5f2eeab018a2e2c6dad32e0c552877
jessiLeandro <jessi.profissional@gmail.com>
2019-09-17 17:58:08 -0300

    updateUser

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -98,3 +98,28 @@ export const NovoUsuarioService = async (values) => {
   })
   return response
 }
+
+export const updateUsuario = async (values) => {
+  const storeObject = store.getState()
+
+  const headers = {
+    token: storeObject.auth.token,
+    username: storeObject.auth.username,
+  }
+
+  let response = {}
+
+  await axios.put(`${BACKEND_URL}/api/user`, values, { headers: headers }).then(
+    resp => {
+      response = resp
+    }
+  ).catch((error) => {
+    if (error.response) {
+      response = error.response
+    } else {
+      console.log('Error', error.message);
+    }
+  })
+  return response
+}
+
