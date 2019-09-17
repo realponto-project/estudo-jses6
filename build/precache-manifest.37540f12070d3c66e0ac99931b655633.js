c2826636d218c3e9155b0ef219f660886b19728f
cintiatilly <joannisbs@gmail.com>
2019-09-16 21:49:45 -0300

    ADD: server

---
+++ build/precache-manifest.37540f12070d3c66e0ac99931b655633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8af78567d07012abc6a1fc3fe0012b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b4bcaea1c928a3df46",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "44508a55c0ce16af1b5f",
+    "url": "/static/css/main.6d96139b.chunk.css"
+  },
+  {
+    "revision": "e7b4bcaea1c928a3df46",
+    "url": "/static/js/2.9c21763f.chunk.js"
+  },
+  {
+    "revision": "44508a55c0ce16af1b5f",
+    "url": "/static/js/main.6498769c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- src/services/var.js
+++ src/services/var.js
@@ -1,5 +1,5 @@
-export const BACKEND_URL = 'http://192.168.0.131:5302'
+// export const BACKEND_URL = 'http://192.168.0.131:5302'
 
 // export const BACKEND_URL = 'http://192.168.0.221:5302'
 
-// export const BACKEND_URL = 'http://200.196.235.175:5312'
+export const BACKEND_URL = 'http://200.196.235.175:5312'
