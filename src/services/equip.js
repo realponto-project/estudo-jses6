a9ef30307cf485b0dc02b0ef7ba9d70697bdc69d
Jessi Leandro Castro <JLC@localhost.localdomain>
2020-01-07 16:09:49 -0300

    fix

--- src/services/equip.js
+++ src/services/equip.js
@@ -1,27 +1,53 @@
-import axios from 'axios'
-import { BACKEND_URL } from './var'
-import { store } from '../App'
+import axios from "axios";
+import { BACKEND_URL } from "./var";
+import { store } from "../App";
 
-export const addEquip = async valeu => {
-    const storeObject = store.getState()
-  
-    const headers = {
-      token: storeObject.auth.token,
-      username: storeObject.auth.username,
-    }
-  
-    let response = {}
-  
-    await axios.post(`${BACKEND_URL}/api/equip`, valeu,  { headers: headers}).then(
-      resp => {
-        response = resp
-      }
-    ).catch((error) => {
-      if (error.response) {
-        response = error.response
-      } else {
-        console.log('Error', error.message);
-      }
+export const addEquip = async value => {
+  const storeObject = store.getState();
+
+  const headers = {
+    token: storeObject.auth.token,
+    username: storeObject.auth.username
+  };
+
+  let response = {};
+
+  await axios
+    .post(`${BACKEND_URL}/api/equip`, value, { headers: headers })
+    .then(resp => {
+      response = resp;
     })
-    return response
-  }
+    .catch(error => {
+      if (error.response) {
+        response = error.response;
+      } else {
+        console.log("Error", error.message);
+      }
+    });
+  return response;
+};
+
+export const getAllEquipsService = async query => {
+  const storeObject = store.getState();
+
+  const headers = {
+    token: storeObject.auth.token,
+    username: storeObject.auth.username
+  };
+
+  let response = {};
+
+  await axios
+    .get(`${BACKEND_URL}/api/equip`, { headers: headers, params: { query } })
+    .then(resp => {
+      response = resp;
+    })
+    .catch(error => {
+      if (error.response) {
+        response = error.response;
+      } else {
+        console.log("Error", error.message);
+      }
+    });
+  return response;
+};
