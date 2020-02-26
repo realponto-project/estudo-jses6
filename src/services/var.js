1e590ff623be9600c231283f194aa21f430ec5c1
guilhermestain <guilhermestain@gmail.com>
2020-02-26 14:18:11 -0300

    fix select

--- src/services/var.js
+++ src/services/var.js
@@ -1,3 +1,5 @@
 // export const BACKEND_URL = "https://radiant-wildwood-95631.herokuapp.com";
 
-export const BACKEND_URL = "http://200.196.235.175:5312";
+export const BACKEND_URL = "http://192.168.90.211:5312";
+
+// export const BACKEND_URL = "http://200.196.235.175:5312";
